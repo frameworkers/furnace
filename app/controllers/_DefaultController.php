5fd997f515a0453a2080263b57a8c5832c92614b
Andrew Hart <andrew@frameworkers.org>
2008-07-25 06:48:44 +0000

    created application default controller and view

---
+++ app/controllers/_DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * frameworkers_furnace
+ * 
+ * _DefaultController.php
+ * Created on Jul 24, 2008
+ *
+ * Copyright 2008 Frameworkers.org.
+ * http://www.frameworkers.org
+ */
+ class _DefaultController extends FPage {
+ 	
+ 	public function index() {
+ 		
+ 		
+ 	}
+ 	
+ }
+?>
