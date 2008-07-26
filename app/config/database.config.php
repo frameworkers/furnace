8a898d5a91043c33ce154d902c05f05af9e94fb1
Andrew Hart <andrew@frameworkers.org>
2008-07-26 03:24:52 +0000

    simplified FDatabaseConfig (drastically... it may still need some work to improve user-friendliness)

--- app/config/database.config.php
+++ app/config/database.config.php
@@ -10,57 +10,21 @@
  */
  
  /*
-  * Class: MDB2_Config
+  * Class: FDatabaseConfig
   * 
   * This class defines a data source name (DSN) for
-  * connecting to a database using PEAR MDB2. If you
-  * wish to use another database engine, you must 
-  * create a similar class named @@@@_Config, and 
-  * extending FDatabaseConfig. @@@@ should be whatever
-  * you set DB_ENGINE to in 'project.config.php'.
+  * connecting to a database using PEAR MDB2. the 
+  * default engine supported by Furnace.
   * 
   */
- class MDB2_Config extends FDatabaseConfig {
- 	
- 	public function __construct() {
- 		$this->db_user = "user";
- 		$this->db_pass = "password";
- 		$this->server  = "localhost";
- 		$this->db_name = "databasename";		
- 	}
- 	
- 	public function getDSN() {
- 		return "mysql://{$this->db_user}:{$this->db_pass}"
- 			."@{$this->server}/{$this->db_name}";		
- 	}
- }
- 
- 
- /************************************************************************
-  * NO USER EDITABLE CODE BELOW THIS LINE
-  ***********************************************************************/
- /*
-  * Class: FDatabaseConfig 
-  *
-  * This class provides a basis for creating flexible data source names
-  * (DSN's) suitable for connecting to different databases.
-  */
- abstract class FDatabaseConfig {
- 	
- 	private $db_user = '';
- 	private $db_pass = '';
- 	private $server  = '';
- 	private $db_name = '';
- 	
- 	public function __construct($user,$pass,$server,$db_name) {
- 		
- 		// Assign provided values
- 		$this->db_user = $user;
- 		$this->db_pass = $pass;
- 		$this->server  = $server;
- 		$this->db_name = $db_name;
- 	}	
- 	 	
- 	public abstract function getDSN();
+ class FDatabaseConfig {
+ 	// You should change these values to be appropriate
+	// for your project environment.
+	//  user: the database username
+	//  password: the password for 'user'
+	//  server: the database server (usually 'localhost')
+	//  dbname: the name of the database to connect to
+	//  mysql:  use mdb2's mysql driver (default)
+ 	public static $DSN = "mysql://user:password@server/dbname";
  }
 ?>
