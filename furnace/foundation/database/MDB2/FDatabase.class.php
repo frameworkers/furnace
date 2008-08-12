ca9be48a08285ee1997fa6c61691ab2294f32cc0
Andrew Hart <andrew@frameworkers.org>
2008-08-12 05:54:32 +0000

    rewrote the MDB2 FDatabase class to include benchmarking hooks.

--- furnace/foundation/database/MDB2/FDatabase.class.php
+++ furnace/foundation/database/MDB2/FDatabase.class.php
@@ -16,9 +16,87 @@
   * 
   */
   require_once("MDB2.php");
-  class FDatabase extends MDB2 {
-		
-	/* No custom extensions at the moment */
+  class FDatabase  {
   	
+  	private $mdb2;
+  	
+  	private function __construct() {
+  		$this->mdb2 = MDB2::singleton(FDatabaseConfig::$DSN);
+  	}
+	
+	public static function singleton() {
+		static $db;
+		
+		if (!isset($db)) {
+			$c  = __CLASS__;
+			$db = new $c();	
+		}	
+		
+		return $db;
+	}
+		
+	public function query($q) {
+		if (FProject::DEBUG_LEVEL == 2) {
+			$bm_start = microtime(true);	
+		}	
+		$r = $this->mdb2->query($q);
+		if (FProject::DEBUG_LEVEL == 2) {
+			$bm_end   = microtime(true);
+			$GLOBALS['queries'][] = array( 
+				'sql'   => $q,
+				'delay' => $bm_end - $bm_start
+			);	
+		}
+		return $r;		
+	}
+	
+	public function queryRow($q) {
+		if (FProject::DEBUG_LEVEL == 2) {
+			$bm_start = microtime(true);	
+		}	
+		$r = $this->mdb2->queryRow($q);
+		if (FProject::DEBUG_LEVEL == 2) {
+			$bm_end   = microtime(true);
+			$GLOBALS['queries'][] = array( 
+				'sql'   => $q,
+				'delay' => $bm_end - $bm_start
+			);	
+		}
+		return $r;		
+	}
+	
+	public function exec($q) {
+		if (FProject::DEBUG_LEVEL == 2) {
+			$bm_start = microtime(true);	
+		}	
+		$r = $this->mdb2->exec($q);
+		if (FProject::DEBUG_LEVEL == 2) {
+			$bm_end   = microtime(true);
+			$GLOBALS['queries'][] = array( 
+				'sql'   => $q,
+				'delay' => $bm_end - $bm_start
+			);	
+		}
+		return $r;		
+	}
+	public function lastInsertID() {
+		if (FProject::DEBUG_LEVEL == 2) {
+			$bm_start = microtime(true);	
+		}	
+		$r = $this->mdb2->lastInsertID($q);
+		if (FProject::DEBUG_LEVEL == 2) {
+			$bm_end   = microtime(true);
+			$GLOBALS['queries'][] = array( 
+				'sql'   => 'LAST INSERT ID',
+				'delay' => $bm_end - $bm_start
+			);	
+		}
+		return $r;		
+	}
+	
+	public function setFetchMode($mode) {
+		return $this->mdb2->setFetchMode($mode);	
+	}
   }
+
 ?>
