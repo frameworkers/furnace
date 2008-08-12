fa03cdc21ccaad894eb18be0acf151b12bbdc11a
Andrew Hart <andrew@frameworkers.org>
2008-08-12 05:39:00 +0000

    added base controller class. All controllers should extend this class instead of FController. Doing so will allow for common functionality to be abstracted away and made available to all controllers in one location. (Authorization checks are a good example of where this might be useful)

---
+++ app/controllers/_base/Controller.class.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * frameworkers_furnace
+ * 
+ * Controller.class.php
+ * Created on Jul 27, 2008
+ *
+ * Copyright 2008 Frameworkers.org. 
+ * http://www.frameworkers.org
+ */
+
+abstract class Controller extends FController {
+	
+	
+	/**
+	 * Here you can define functions that should be accessible
+	 * to ALL controllers. 
+	 */
+
+	
+	
+	
+	
+}
+?>
