05d35a7e155d3d9b19d287a201d2342f9f4b9c58
Andrew Hart <andrew@frameworkers.org>
2008-10-16 04:06:44 +0000

    added the _FurnaceController class which handles customized views for Furnace-related pages. Currently implemented are:  -- debug: a page to show debug messages (no such controller/view,etc)  -- unavailable: a default page to show when the site is unavailable     (this can be configured in /app/config/routes.yml)

---
+++ app/controllers/_furnaceController.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * frameworkers_furnace
+ * 
+ * _DefaultController.php
+ * Created on Oct 14, 2008
+ *
+ * Copyright 2008 Frameworkers.org.
+ * http://www.frameworkers.org
+ */
+class _FurnaceController extends Controller {
+	
+	public function debug($message) {
+		
+		$this->set('message',$message);
+		
+	}
+	
+	public function unavailable() {
+		
+	}
+	
+	
+}
+?>
