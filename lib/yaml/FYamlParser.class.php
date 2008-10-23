b8bb1ced1e6e5acb6e61c0a7d27fffcc667531b1
Andrew Hart <andrew@frameworkers.org>
2008-10-23 04:23:16 +0000

    reinstate FYamlParser.class.php which got lost in the reorganization somehow

---
+++ lib/yaml/FYamlParser.class.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ * frameworkers-foundation
+ * 
+ * FYamlParser.php
+ * Created on May 17, 2008
+ *
+ * Copyright 2008 Frameworkers.org. 
+ * http://www.frameworkers.org
+ */
+ 
+ /*
+  * Class: FYamlParser
+  * A class to parse YAML strings (or files) using the Simple
+  * PHP YAML Class (http://spyc.sourceforge.net).
+  */
+class FYamlParser extends Spyc {	
+ 	
+ 	/*
+ 	 * Function: parse
+ 	 * 
+ 	 * Parses a YAML string (or file) into a PHP array object.
+ 	 * 
+ 	 * Parameters: 
+ 	 * 	
+ 	 * 	yml - a string or file path containing YAML markup.
+ 	 * 
+ 	 * Returns: 
+ 	 * 
+ 	 * A PHP array object containing the parsed data.
+ 	 */
+ 	public static function parse($yml) {
+ 		// Convert the YAML into an array
+		return Spyc::YAMLLoad($yml);
+ 	}
+ }
+?>
