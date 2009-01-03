415e605744fe6945883b67e019c807ed272e0821
Andrew Hart <andrew@frameworkers.org>
2009-01-03 16:00:47 +0000

    added 'DataController' and 'RolesController' to the Fuel application  -- DataController handles programmatic application data management (c/r/u/d)  -- RolesController is an experimental area devoted to application role management

---
+++ lib/fuel/app/controllers/dataController.php
@@ -0,0 +1,219 @@
+<?php
+class DataController extends Controller {
+	
+	public function index() {
+		if ($GLOBALS['fconfig_debug_level'] > 0 && 
+			$GLOBALS['fconfig_debug_dsn'] == 'mysql://user:password@server/dbname') {
+			die("No debug database specified. Please edit the 'fconfig_debug_dsn' variable in your application config file");
+		} else if ($GLOBALS['fconfig_debug_level'] == 0 &&
+				   $GLOBALS['fconfig_production_dsn'] == 'mysql://user:password@server/dbname') {
+			die("No production database specified. Please edit the 'fconfig_production_dsn' variable in your application config file");	   	
+		}
+		
+		$this->init();		// Load required files
+		
+		$d = new FDatabaseSchema();
+		if ($GLOBALS['fconfig_debug_level'] > 0) {
+			$d->discover($GLOBALS['fconfig_debug_dsn']);
+		} else {
+			$d->discover($GLOBALS['fconfig_production_dsn']);
+		}
+		$model = $this->getModel();
+		
+		$this->set('objects',$model->objects);
+	}
+	
+	public function objects($name) {
+		$this->init();		// Load required files
+		
+		$d = new FDatabaseSchema();
+		if ($GLOBALS['fconfig_debug_level'] > 0) {
+			$d->discover($GLOBALS['fconfig_debug_dsn']);
+		} else {
+			$d->discover($GLOBALS['fconfig_production_dsn']);
+		}
+		$model = $this->getModel();
+		
+		$object = false;
+		foreach ($model->objects as $o) {
+			if ($o->getName() == $name) {
+				$object = $o;
+			}
+		}
+		if (!$object) {
+			$this->flash("Object type '{$name}' not found in the database.","error");
+			$this->redirect("/fuel/data/");
+		}
+		
+		$this->set('object',$object);
+		
+		// Create an FObjectCollection object to manage object retrieval
+		$coll_class = "{$name}Collection";
+		$objectCollection = new $coll_class();
+		
+		// Load objects based on pagination instructions
+		if (isset($_GET['page'])) {
+			$objects = $objectCollection->getPage($_GET['page'],10,$_GET['sortBy'],$_GET['sortOrder']);
+		} else {
+			$objects = $objectCollection->getPage(1,10,"objId","desc");
+		}
+		
+		// Build data array from loaded objects
+		$headers = array("objId");
+		$object_datas = array();
+		foreach ($objects as $o) {
+			$object_datas[$o->getObjId()][] = "<a href=\"{$GLOBALS['fconfig_url_base']}/fuel/data/object/{$name}/{$o->getObjId()}\">{$o->getObjId()}</a>";
+		}
+		foreach ($object->getAttributes() as $attr) {
+			$attr_name    = $attr->getName();
+			$attr_fn_name = "get{$attr->getFunctionName()}"; 
+			$headers[] = $attr_name;
+			foreach ($objects as $o) {
+				$object_datas[$o->getObjId()][] = $o->$attr_fn_name();
+			}
+		}
+		foreach ($object->getSockets() as $sock) {
+			if ($sock->getQuantity() == "1") {
+				$headers[] = "{$sock->getName()} ({$sock->getForeign()})";
+				$sock_fn_name = "get{$sock->getFunctionName()}";
+				foreach ($objects as $o) {
+					$object_datas[$o->getObjId()][] = "<a href=\"{$GLOBALS['fconfig_url_base']}/fuel/data/object/{$sock->getForeign()}/{$o->$sock_fn_name()->getObjId()}\">{$o->$sock_fn_name()->getObjId()}</a>";
+				}
+			}
+		}
+		foreach ($objects as $o) {
+			$object_datas[$o->getObjId()][] = "<a style=\"color:red;\"href=\"{$GLOBALS['fconfig_url_base']}/fuel/data/delete/{$name}/{$o->getObjId()}\" onclick=\"return confirm('This action can not be undone. Continue?');\">Delete!</a>";
+		}
+		
+		$this->set('headers',$headers);
+		$this->set('obj_data',$object_datas);
+		$this->set('objectClass',$object);
+		
+		
+		// Register pagination details
+		$this->set('objPagination',$objectCollection->getPaginationData());
+		$this->set('currentPage',(isset($_GET['page'])? $_GET['page'] : 1));
+	}
+
+	public function object($class,$id) {
+		$this->init();		// Load required files
+		
+		$d = new FDatabaseSchema();
+		if ($GLOBALS['fconfig_debug_level'] > 0) {
+			$d->discover($GLOBALS['fconfig_debug_dsn']);
+		} else {
+			$d->discover($GLOBALS['fconfig_production_dsn']);
+		}
+		$model = $this->getModel();
+		
+		$object = false;
+		foreach ($model->objects as $o) {
+			if ($o->getName() == $class) {
+				$object = $o;
+			}
+		}
+		if (!$object) {
+			$this->flash("Object type '{$class}' not found in the database.","error");
+			$this->redirect("/fuel/data/");
+		}
+		
+		// Create an FObjectCollection object to manage object retrieval
+		$coll_class = "{$class}Collection";
+		$objectCollection = new $coll_class();
+		$the_object = $objectCollection->get($id);
+		$this->set('objectClass',$object);
+		$this->set('object',$the_object);
+		
+		$headers = array();
+		$data    = array();
+	}
+	
+	public function create() {
+		if ($this->form) {
+			
+			$this->init();		// Load required files
+		
+			$d = new FDatabaseSchema();
+			if ($GLOBALS['fconfig_debug_level'] > 0) {
+				$d->discover($GLOBALS['fconfig_debug_dsn']);
+			} else {
+				$d->discover($GLOBALS['fconfig_production_dsn']);
+			}
+			$model = $this->getModel();
+			
+			$objectType = $this->form['fobjectType'];
+			$object = false;
+			foreach ($model->objects as $o) {
+				if ($o->getName() == $objectType) {
+					$object = $o;
+				}
+			}
+			if (!$object) {
+				$this->flash("Object type '{$objectType}' not found in the database.","error");
+				$this->redirect("/fuel/data/");
+			}
+			
+			$params = array();
+			if ("FAccount" == $object->getParentClass()) {
+				$params[]  = $this->form['username'];
+				$params[]  = $this->form['password'];
+				$params[]  = $this->form['emailAddress']; 
+			}
+			foreach ($object->getAttributes() as $attr) {
+				if ($attr->isUnique()) {
+					$params[] = $attr->getName();
+				}
+			}
+			
+			$o = new $objectType(false);
+			$obj = call_user_func_array(array($objectType,"Create"),$params);
+			foreach ($object->getAttributes() as $attr) {
+				$set = "set{$attr->getName()}";
+				$obj->$set($this->form[$attr->getName()],false);
+			}
+			$obj->save();
+			
+			
+			$this->flash("Created new {$objectType} object!");
+			$this->redirect("/fuel/data/objects/{$objectType}");
+		}
+	}
+	
+	public function delete($class,$id) {
+		call_user_func(array($class,"Delete"),$id);
+		$this->flash("Deleted '{$class}' object with id {$id}.");
+		$this->redirect("/fuel/data/objects/{$class}");
+	}
+	
+	
+	private function init() {
+		require_once($GLOBALS['fconfig_root_directory'] . "/lib/furnace/foundation/database/MDB2/FDatabase.class.php");
+		require_once($GLOBALS['fconfig_root_directory'] . "/lib/fuel/lib/generation/core/FObj.class.php");
+		require_once($GLOBALS['fconfig_root_directory'] . "/lib/fuel/lib/generation/core/FObjAttr.class.php");
+		require_once($GLOBALS['fconfig_root_directory'] . "/lib/fuel/lib/generation/core/FObjSocket.class.php");
+		require_once($GLOBALS['fconfig_root_directory'] . "/lib/fuel/lib/generation/core/FSqlColumn.class.php");
+		require_once($GLOBALS['fconfig_root_directory'] . "/lib/fuel/lib/generation/core/FSqlTable.class.php");
+		require_once($GLOBALS['fconfig_root_directory'] . "/lib/fuel/lib/generation/building/FModel.class.php");
+		require_once($GLOBALS['fconfig_root_directory'] . "/lib/fuel/lib/dbmgmt/FDatabaseSchema.class.php");
+	}
+	private function getModel() {
+		return new FModel(
+			FYamlParser::parse(
+				file_get_contents($GLOBALS['fconfig_root_directory'] . "/app/model/model.yml")
+			)
+		);
+	}
+	private function getSchema() {
+		$d = new FDatabaseSchema();
+		if ($GLOBALS['fconfig_debug_level'] > 0) {
+			$d->discover($GLOBALS['fconfig_debug_dsn']);
+		} else {
+			$d->discover($GLOBALS['fconfig_production_dsn']);
+		}
+		return $d;
+	}
+	
+	
+	
+}
+?>
---
+++ lib/fuel/app/controllers/rolesController.php
@@ -0,0 +1,86 @@
+<?php
+class RolesController extends Controller {
+	
+	public function index() {
+		
+		//$q = "SELECT * FROM `app_accounts`,`app_roles` WHERE `app_accounts`.`objId`=`app_roles`.`accountId` ";
+		//$users_results = _db()->query($q);
+		
+		$q= "DESCRIBE `app_roles`";
+		$roles_results = _db()->query($q);
+
+		$defined_roles = array();
+		while ($role = $roles_results->fetchRow()) {
+			if ($role[0] == "accountId") {continue;}
+			$defined_roles[] = $role[0];
+		}
+		
+		$users = User::Retrieve();
+		
+		$this->set('defined_roles',$defined_roles);
+		$this->set('users',$users);
+		
+	}
+	
+	public function createUser($fn,$ln,$un,$pw,$email) {
+
+ 		// Create a user object to go along with the account
+		$user = User::Create($un,$pw,$email);
+ 		//$user = User::Create(FAccountManager::Create($un,$pw,$email));
+ 		$user->setFirstName($fn,false);
+ 		$user->setLastName($ln,false);
+ 		$user->save();
+ 		
+ 		// Redirect the user to the login screen
+ 		$this->redirect("/login");
+ 	}
+ 	
+ 	public function deleteUser($objId) {
+ 		User::Delete($objId);
+ 		$this->redirect("/fuel/roles/");
+ 	}
+ 	
+ 	public function createAccount() {
+ 		if ($this->form) {
+ 			$id = FAccountManager::Create($this->form['username'],$this->form['password'],$this->form['email']);
+ 			$this->flash("A new account was created for user '{$this->form['username']}' with unique id '{$id}'");
+ 		} else {
+ 			$this->flash("Could not create account. Try POSTing the data.","error");
+ 		}
+ 		$this->redirect("/fuel/roles");
+ 	}
+ 	
+ 	public function defineRole($name,$default) {
+ 		if (_user("/login")) {
+ 			_account()->requireRole("administrator","/login");
+ 			FAccount::DefineRole($name,$default);
+ 			$this->redirect("/");
+ 		}
+ 	}
+ 	
+ 	public function deleteRole($name) {
+ 		if (_user("/login")) {
+ 			_account()->requireRole("administrator","/login");
+ 			FAccount::DeleteRole($name);
+ 			$this->redirect("/");
+ 		}
+ 	}
+ 	
+ 	public function getPower() {
+ 		if (false !== ($user = $this->requireLogin("/login"))) {
+ 			// Make the currently logged in user an Administrator
+			$user->getFAccount()->grantRole("administrator");
+			echo "SUPERP0WER!";
+			die(); 			
+ 		}
+ 	}
+ 	public function relinquishPower() {
+ 	 	if (false !== ($user = $this->requireLogin("/login"))) {
+ 			// Make the currently logged in user an Administrator
+			$user->getFAccount()->denyRole("administrator");
+			echo "GRACIOUSLY ABDICATING P0WER";
+			die(); 			
+ 		}
+ 	}
+}
+?>
