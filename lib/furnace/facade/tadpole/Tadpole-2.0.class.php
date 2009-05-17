543b9e05e498db4682cab5499cd3ae9ab4e2e3a8
Andrew Hart <andrew@frameworkers.org>
2009-05-17 01:15:15 +0000

    TADPOLE: added new option for 'type' command: nicedate
    nicedate formats a date as a string in the form of '3 hours ago', '45 minutes ago', or '2 weeks from now'
    
    Useage:
    
    [member.lastLogin;type=nicedate]

--- lib/furnace/facade/tadpole/Tadpole-2.0.class.php
+++ lib/furnace/facade/tadpole/Tadpole-2.0.class.php
@@ -489,6 +489,9 @@ class Tadpole {
 								? $commands['format']
 								: 'Y-m-d G:i:s'),strtotime($value));
 							break;
+						case 'nicedate':
+							$value = $this->nicedate($value);
+							break;
 						case 'usphone':
 							$value = str_pad($value,11,"1", STR_PAD_LEFT);
 							$value = "{$value[0]}-".substr($value,1,3).'-'.substr($value,4,3).'-'.substr($value,7);
@@ -784,6 +787,41 @@ class Tadpole {
 		}
 	}
 	
+	public function nicedate($date){
+   
+	    $periods         = array("second", "minute", "hour", "day", "week", "month", "year", "decade");
+	    $lengths         = array("60","60","24","7","4.35","12","10");
+	   
+	    $now             = time();
+	    $unix_date       = strtotime($date);
+	   
+	    // check validity of date
+	    if(empty($unix_date)) {   
+	        return 'unknown';
+	    }
+
+	    // is it future date or past date
+	    if($now > $unix_date) {   
+	        $difference     = $now - $unix_date;
+	        $tense         = "ago";
+	       
+	    } else {
+	        $difference     = $unix_date - $now;
+	        $tense         = "from now";
+	    }
+   
+	    for($j = 0; $difference >= $lengths[$j] && $j < count($lengths)-1; $j++) {
+	        $difference /= $lengths[$j];
+	    }
+   
+    	$difference = round($difference);
+   
+	    if($difference != 1) {
+	        $periods[$j].= "s";
+	    }
+   
+	    return "$difference $periods[$j] {$tense}";
+	}
 	
 	public function set($key,$value) {
 		$this->page_data[$key] = $value;
