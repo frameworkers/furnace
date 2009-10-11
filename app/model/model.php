f73807f70d98b67598e696682c6fe0ef705b081f
Andrew Hart <andrew@frameworkers.org>
2009-10-11 14:48:24 +0000

    Core reorganization to improve model extensibility - added default application model file

---
+++ app/model/model.php
@@ -0,0 +1,9 @@
+<?php
+class ApplicationModel {
+	
+	// Default Application Model. use FUEL to manage your application's model.
+	// Changes made to this file will be overwritten whenever your model is rebuilt.
+	// 
+	
+}
+?>
