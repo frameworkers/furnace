756e390366ad7f6dc18381bad01c68751d572ddb
Andrew Hart <andrew@frameworkers.org>
2010-01-29 22:37:38 +0000

    Added noexist (http 404) and noauth (http403) functions to the default controller. Also made the location of the customizable views for each error page more intuitive.

--- src/app/pages/Controller.class.php
+++ src/app/pages/Controller.class.php
@@ -6,5 +6,14 @@ class Controller extends FController {
     // application controllers in this file.
     
   
+    // Convenience (short-cut) function for 404 errors
+    protected function notfound() {
+        $this->internalRedirect("/_default/http404");
+    }
+    
+    // Convenience (short-cut) function for 403 errors
+    protected function noauth() {
+        $this->internalRedirect("/_default/http403");
+    }
 }
 ?>
--- src/app/pages/_default/_defaultController.php
+++ src/app/pages/_default/_defaultController.php
@@ -1,12 +1,29 @@
 <?php
 class _DefaultController extends Controller {
     
-    
+    // Application home (index) page. If the default routing table
+    // (see: /app/config/routes.yml) is unchanged, this function will be
+    // invoked on requests for '/', aka the home page.
     public function index() {
         
-        $this->set('host',$_SERVER['HTTP_HOST']);
+        //
+        // TODO: prepare data to be passed to the corresponding view
+        //
 
     }
-}
-?>
 
+    
+    // Error page to display on 404 errors
+    // A 404 error can be "forced" from any controller
+    // by using the convenience function $this->noexist(). See
+    // /app/pages/Controller.class.php for the function definition
+    public function http404() { }
+    
+    // Error page to display on 403 errors
+    // A 403 error can be "forced" from any controller
+    // by using the convenience function $this->noauth(). See
+    // /app/pages/Controller.class.php for the function definition
+    public function http403() { }
+}
+
+?>
