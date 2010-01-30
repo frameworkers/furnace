77bfef41511af48338fc91132b9d6066bfa1cf8a
Andrew Hart <andrew@frameworkers.org>
2010-01-30 06:29:14 +0000

    add a provider folder for frameworkers.org and put a sample helper class inside. The MenuListHelper class generates an html unordered list from a specially crafted array of menu items:
    $menuItems = array(
    	array("item1Label","/item1/href","item1 tooltip description",array(..item1 sub items..))
    	...
    );
    
    will become:
    
    <ul>
      <li><a href="/item1/href" title="item1 tooltip description">item1Label</a></li>
      ...
    </ul>
    
    If an item has sub-items, those will be expanded into a nested <ul/> object. The helper even does rudimentary matching on the "href" attributes to determine which (if any) should be considered the "active" element. If one is active, its <li> has the class "active": <li class="active".../>

---
+++ src/app/scripts/helpers/org.frameworkers/menuListHelper.php
@@ -0,0 +1,46 @@
+<?php
+// Renders an HTML list from the provided data array
+class MenuListHelper extends FPageFragment {
+    
+    private $menuItems;
+    private $id;
+    private $class;
+    
+    public function __construct($menuItems = array()) {
+        $this->menuItems = $menuItems;
+    }
+    
+    public function setId($id) {
+        $this->id = $id;
+    }
+    
+    public function setClass($className) {
+        $this->class = $className;
+    }
+    
+    public function render() {
+
+        $menu = "<ul id=\"{$this->id}\" class=\"{$this->class}\">";
+        foreach ($this->menuItems as $mi) {
+            // Determine if this is the active top-level menu item
+            $active = (_furnace()->request == $mi[1])
+                ? 'class="active"'
+                : '';
+            // Draw the item and any nested sub items
+            $menu .= "<li {$active}><a href=\"{$mi[1]}\" title=\"{$mi[2]}\">{$mi[0]}</a>";
+            // Handle nested menus
+            if (is_array($mi[3]) && count($mi[3]) > 0) {
+                $menu .= '<ul>';
+                foreach ($mi[3] as $nmi) {
+                    $menu .= "<li><a href=\"{$nmi[1]}\" title=\"{$nmi[2]}\">{$nmi[0]}</a>";
+                }
+                $menu .= '</ul>';
+            }
+            $menu .= '</li>';
+        }
+        $menu .= '</ul>';
+        
+        return $menu;
+    }
+}
+?>
