541e391b1c52500e15508b46c99750f145b5f371
Andrew Hart <andrew@frameworkers.org>
2010-04-24 03:28:04 +0000

    consistently detect problems connecting to the database in FUEL

--- src/app/plugins/extensions/org.frameworkers.fuel/pages/data/dataController.php
+++ src/app/plugins/extensions/org.frameworkers.fuel/pages/data/dataController.php
@@ -13,73 +13,82 @@ class DataController extends Controller {
     public function index() {
         $this->init();		// Load required files
 		
-		$d = new FDatabaseSchema();
-		$d->discover('default');
-		$model = $this->getModel();
-		
-		$tables = array();
-		$notices= array();
 		
 		
-		// Analyze differences DATABASE vs MODEL
-		foreach ($model->tables as $mt) {
-			$tables[strtolower($mt->getName())] = array("name"=> $mt->getName(),"found"=> false,"status"=> false);
-
-			foreach ($d->getTables() as $dt) {
-				if (strtolower($dt->getName()) == strtolower($mt->getName())) {
-					$tables[strtolower($mt->getName())] = array('name'=> $mt->getName(),'found' => true,'table'=>$mt,'status'=>'ok');
-					// Now that we found the matching table, check the fields for differences
-					if (count($mt->getColumns()) != count($dt->getColumns())){
-						$tables[strtolower($mt->getName())]['status'] = 'fieldsDiffer';
-						break;
-					}
-					foreach ($mt->getColumns() as $mtc) {
-						$found = false;
-						foreach ($dt->getColumns() as $dtc) {
-							if (strtolower($mtc->getName()) == strtolower($dtc->getName())){
-								$found=true;	
-								break;
-							}
-						}
-						if (!$found || ( 
-							($dtc->getColType()      != $mtc->getColType()) ||
-						    ($dtc->isNull()          != $mtc->isNull())     ||
-						    ($dtc->getDefaultValue() != $mtc->getDefaultValue())
-						)) {
-							$tables[strtolower($mt->getName())]['status'] = 'fieldsDiffer';
-							break;
-						}
-					}	
-					break;
-				}
-			}
+        try {
+		    $d = new FDatabaseSchema();
+    		$d->discover('default');
+    		$model = $this->getModel();
+    		
+    		$tables = array();
+    		$notices= array();
+		    $this->set('dbConnectOk',true);
+		} catch (FDatabaseException $fde) {
+		    $this->set('dbConnectOk',false);
+		    $this->set('dbConnectMessage',$fde->getMessage());
 		}
 		
-		// Analyze differences MODEL vs DATABASE
-		foreach ($d->getTables() as $dt) {
-			
-			$bFound = false;
-			foreach ($model->tables as $mt) {
-				if (strtolower($mt->getName()) == strtolower($dt->getName())) {
-					$tables[strtolower($mt->getName())]['found'] = true;
-					$bFound = true;
-					break;
-				}
-			}
-			if (!$bFound) {
-				$notices[] = $dt;
-			}
+		if ($this->page_data['dbConnectOk'] == true) {
+    		// Analyze differences DATABASE vs MODEL
+    		foreach ($model->tables as $mt) {
+    			$tables[strtolower($mt->getName())] = array("name"=> $mt->getName(),"found"=> false,"status"=> false);
+    
+    			foreach ($d->getTables() as $dt) {
+    				if (strtolower($dt->getName()) == strtolower($mt->getName())) {
+    					$tables[strtolower($mt->getName())] = array('name'=> $mt->getName(),'found' => true,'table'=>$mt,'status'=>'ok');
+    					// Now that we found the matching table, check the fields for differences
+    					if (count($mt->getColumns()) != count($dt->getColumns())){
+    						$tables[strtolower($mt->getName())]['status'] = 'fieldsDiffer';
+    						break;
+    					}
+    					foreach ($mt->getColumns() as $mtc) {
+    						$found = false;
+    						foreach ($dt->getColumns() as $dtc) {
+    							if (strtolower($mtc->getName()) == strtolower($dtc->getName())){
+    								$found=true;	
+    								break;
+    							}
+    						}
+    						if (!$found || ( 
+    							($dtc->getColType()      != $mtc->getColType()) ||
+    						    ($dtc->isNull()          != $mtc->isNull())     ||
+    						    ($dtc->getDefaultValue() != $mtc->getDefaultValue())
+    						)) {
+    							$tables[strtolower($mt->getName())]['status'] = 'fieldsDiffer';
+    							break;
+    						}
+    					}	
+    					break;
+    				}
+    			}
+    		}
+    		
+    		// Analyze differences MODEL vs DATABASE
+    		foreach ($d->getTables() as $dt) {
+    			
+    			$bFound = false;
+    			foreach ($model->tables as $mt) {
+    				if (strtolower($mt->getName()) == strtolower($dt->getName())) {
+    					$tables[strtolower($mt->getName())]['found'] = true;
+    					$bFound = true;
+    					break;
+    				}
+    			}
+    			if (!$bFound) {
+    				$notices[] = $dt;
+    			}
+    		}
+    		
+    		$existingTableNames = array();
+    		foreach ($d->getTables() as $t) {
+    			$existingTableNames[] = $t->getName();
+    		}
+    		$this->set("existingTableNames",$existingTableNames);
+    		$this->set("tables",$tables);
+    		$this->set("notices",$notices);
+    		$this->set("theModel",$model);
+    		$this->extensionAddStylesheet('org.frameworkers.fuel','index.css',true);
 		}
-		
-		$existingTableNames = array();
-		foreach ($d->getTables() as $t) {
-			$existingTableNames[] = $t->getName();
-		}
-		$this->set("existingTableNames",$existingTableNames);
-		$this->set("tables",$tables);
-		$this->set("notices",$notices);
-		$this->set("theModel",$model);
-		$this->extensionAddStylesheet('org.frameworkers.fuel','index.css',true);
     }
     
     public function objects($name,$page = 1) {
