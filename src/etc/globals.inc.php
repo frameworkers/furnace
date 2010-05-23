94635f63ce2388ec3c1a1fa58fe020f159917952
Andrew Hart <andrew@frameworkers.org>
2010-05-23 20:37:26 +0000

    cleanup and improve documentation

--- src/etc/globals.inc.php
+++ src/etc/globals.inc.php
@@ -6,62 +6,102 @@ define(FF_CONFIG_FILE, FF_ROOT_DIR . '/app/config/app.yml');
 define(FF_LIB_DIR,     FF_ROOT_DIR . '/lib');
 define(FF_LOG_DIR,     FF_ROOT_DIR . '/app/data/logs');
 
+// Logging Constants (requires the PEAR Log package)
+// http://pear.php.net/package/Log/
+define(FF_DEBUG,       PEAR_LOG_DEBUG);
+define(FF_INFO,        PEAR_LOG_INFO);
+define(FF_NOTICE,      PEAR_LOG_NOTICE);
+define(FF_WARNING,     PEAR_LOG_WARNING);
+define(FF_ERROR,       PEAR_LOG_ERR);
+define(FF_CRIT,        PEAR_LOG_CRIT);
+define(FF_ALERT,       PEAR_LOG_ALERT);
+define(FF_EMERG,       PEAR_LOG_EMERG);
+
 /* GLOBAL DATA STRUCTURES ***************************************************/
 
- $_datasources = array("production"=>array(),"debug"=>array());
- $_logmgr      = false;    // Initialized in {FF_ROOT_DIR}/etc/app.php
+$_datasources = array("production"=>array(),"debug"=>array());
+$_logmgr      = false;    // Initialized in [FF_ROOT_DIR]/etc/app.php
 
 
 /* GLOBAL UTILITY FUNCTIONS  ************************************************/
 
- 
- // FUNCTION: _db()
- //  Provides shorthand notation for accessing the database, and also
- //  insulates against API changes that will probably come as a result
- //  of a future need to support additional database connection mechanisms.
- // 
-
- 
- function _db($which='default') {
+/**
+ * _db
+ * 
+ * Global function that provides shorthand notation for accessing a datasource 
+ * driver and also insulates against API changes that come as a result
+ * of a future need to support additional database connection mechanisms.
+ * 
+ * @param  string $which  The identifier (from app.yml) of the database to use
+ * @return FDatasourceDriver An FDatasourceDriver object that can interact with
+ *         the requested datasource
+ *         
+ * @global 
+ */
+function _db($which='default') {
     global $_datasources;
+    
+    // Determine whether to use the debug or production datasources
+    $debugLevel = (_furnace()->config->debug_level > 0)
+        ? 'debug'
+        : 'production';
+    
+    // If this is the first access, load the appropriate driver and init. it  
+    if (!isset($_datasources[$debugLevel][$which])) {
+        $driverClass = _furnace()->config->data['datasources'][$debugLevel][$which]['driver'];
+        $options     = _furnace()->config->data['datasources'][$debugLevel][$which]['options'];
+        require_once (FF_LIB_DIR . "/furnace/datasources/drivers/{$driverClass}.class.php");
+        
+        $_datasources[$debugLevel][$which] = new $driverClass();
+        $_datasources[$debugLevel][$which]->init($options);  
+    }
+    
+    return $_datasources[$debugLevel][$which];
+}
  
-    if (_furnace()->config['debug_level'] > 0) {
- 	    if (!isset($_datasources['debug'][$which])) {
- 	        $_datasources['debug'][$which] = new FDatabase(_furnace()->config['datasources']['debug'][$which]);
- 	    } 
- 	    
- 	    return $_datasources['debug'][$which];
- 	    
- 	} else {
- 		if (!isset($_datasources['production'][$which])) {
- 	        $_datasources['production'][$which] = new FDatabase(_furnace()->config['datasources']['production'][$which]);
- 	    } 
- 	    
- 	    return $_datasources['production'][$which];
- 	}
- }
- 
- // FUNCTION: _model()
- //  Provides shorthand notation for accessing the application's 
- //  data model.
- function _model() {
-     return $GLOBALS['_model'];
- }
- 
- // FUNCTION: _user()
- //  Provides shorthand notation for accessing the currently logged
- //  in user.
- //
+/**
+ * _model()
+ * 
+ * Global function that provides shorthand notation for accessing the
+ * application's data model
+ * 
+ * @return ApplicationModel   The application's information model
+ * @global
+ */
+function _model() {
+    return $GLOBALS['_model'];
+}
+
+ /**
+  * _user
+  * 
+  * Provides shorthand notation for accessing the currently logged
+  * in user
+  * 
+  * @return  FAccount  The FAccount-derived object representing the user
+  * @global
+  */
  function _user() {
  	return FSessionManager::checkLogin();
  }
  
- // FUNCTION: _err()
- function _err($productionMessage,$debugMessage='',$isFatal = false,$handler='unknown') {
+ 
+ /**
+  * _err
+  * 
+  * Global function that provides a shorthand notation for sending messages
+  * to the built-in 'flashes' (notices) section of the page layout.
+  * 
+  * @param string $productionMessage  The message to display in production
+  * @param string $debugMessage       The (optional) message to display while debugging
+  * @param $isFatal                   Whether or not the error prevents further action
+  * @return nothing
+  */
+ function _err($productionMessage,$debugMessage='',$isFatal = false) {
  	// display the error to the user
  	// if debugging, display the debug message, if defined, else the production message
  	// if not debugging, display the production message
- 	if (0 == _furnace()->config['debug_level']) {
+ 	if (0 == _furnace()->config->debug_level) {
  		// Production Mode
  		if ('' != $productionMessage) {
  			// Display the production message
@@ -74,49 +114,36 @@ define(FF_LOG_DIR,     FF_ROOT_DIR . '/app/data/logs');
  	} else {
  		// Debug Mode
 		$_SESSION['flashes'][] = array(
-			'message' => ((_furnace()->config['debug_level'] > 0) 
-					? (('' == $debugMessage)
-						? $productionMessage 
-						: $debugMessage) 
-					: $productionMessage),
+			'message' => (('' == $debugMessage)    // debug msg defined?
+				? $productionMessage 
+				: $debugMessage),
 			'cssClass'=> 'error',
 			'title'   => 'Error:'
 		);
  	}
  	// log the error (and email?) if fatal
- 	
- 	
  	if ($isFatal) {
+ 	    $msg = ('' == $debugMessage) 
+ 	        ? $productionMessage
+ 	        : $debugMessage;
+ 	    _log()->log($msg,FF_ERROR);
  		die("FATAL ERROR");
  	}
  }
  
- // FUNCTION: _account()
- //  Provides shorthand notation for accessing the currently logged in
- //  user's account information (un,pw,etc.)
- //
- function _account($failPage = '/') {
- 	if (false !== ($user = FSessionManager::checkLogin())) {
- 		return $user->getFAccount();
- 	} else {
- 		header("Location: {$failPage}");
- 		exit;
- 	}
- }
- 
-// FUNCTION: _furnace()
-//  Provides shorthand notation for accessing the furnace object.
- 
+/**
+ * _furnace
+ * 
+ * Global function that provides shorthand notation for accessing the
+ * furnace object
+ * 
+ * @return Furnace   The global furnace object
+ * @global
+ */
 function _furnace() {
  	return $GLOBALS['furnace'];	
 }
 
-// FUNCTION: _model()
-//  Provides shorthand notation for accessing the application model
-//function _model() {
-//	return $GLOBALS['fApplicationModel'];
-//}
- 
 // FUNCTION _storeUserInput($data)
 //   Stores the requested $data in the session variable _userform
 function _storeUserInput($data) {
@@ -141,39 +168,6 @@ function _readUserInput($field) {
 function _log($which = 'default') {
     global $_logmgr;
     return $_logmgr->getLog($which);
-}
-
-
-define(FF_INFO,true);
-define(FF_DEBUG,true);	
-define(FF_WARN,true);
-define(FF_ERR,true);
-
-$dev_messages = array();
-
-function info($msg) {
-	global $dev_messages;
-	$dev_messages[] =  sprintf("%.5f I: %s ",microtime(true),$msg);
-}
-function debug($msg) {
-	global $dev_messages;
-	$dev_messages[] =  sprintf("%.5f D: %s ",microtime(true),$msg);
-}
-function warn($msg) {
-	global $dev_messages;
-	$dev_messages[] =  sprintf("%.5f W: %s ",microtime(true),$msg);
-}
-function err($msg) {
-	global $dev_messages;
-	$dev_messages[] =  sprintf("%.5f E: %s ",microtime(true),$msg);
-}
-function dump($var) {
-	global $dev_messages;
-	$dev_messages[] =  sprintf("%.5f D: ",microtime(true)).print_r($var,true);
-}
-function dev_messages() {
-    global $dev_messages;
-    echo "<ul><li>" . implode('</li><li>',$dev_messages) .'</li></ul>';
 }
 
 ?>
