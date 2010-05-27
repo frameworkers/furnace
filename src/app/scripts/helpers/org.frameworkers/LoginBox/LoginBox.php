a553ee5fffa433586beb6bcdaad88af9d28d148f
Andrew Hart <andrew@frameworkers.org>
2010-05-27 05:49:38 +0000

    implement temporary banning of abusive hosts after too many failed logins

--- src/app/scripts/helpers/org.frameworkers/LoginBox/LoginBox.php
+++ src/app/scripts/helpers/org.frameworkers/LoginBox/LoginBox.php
@@ -3,16 +3,41 @@ class LoginBox extends FPageFragment {
     
     private $errors     = array();
     private $successURI = '/';
+    private $banned     = false;
     
     public function __construct($controller,$successURI) {
         parent::__construct($controller,dirname(__FILE__));
         $this->successURI = $successURI;
         
-        if ($this->controller->form  
+        // Check if this IP address can attempt to log in
+        $this->banned = $this->checkBan();
+        
+        // Process the login request
+        if (!$this->banned && $this->controller->form  
             && isset($this->controller->form['username']) 
             && isset($this->controller->form['password'])) {
             $this->processLogin();
         }
+    }
+    
+    private function checkBan() {
+        // Flush old failed logins
+        //TODO: parameterize this with something like: login_failure_timeout
+        $q = "DELETE FROM `app_logs` WHERE `created` < '" 
+            . date('Y-m-d g:i:s',strtotime('1 hour ago')) . "'";
+        _db()->rawExec($q); 
+        
+        // Check if this IP can attempt to log in
+        //TODO: parameterize this with something like max_login_failures
+		$q = "SELECT COUNT(*) FROM `app_logs` WHERE `ip`='{$_SERVER['REMOTE_ADDR']}' ";
+		$attemptsUsed = _db()->rawQuery($q,array('type'=>'one'));
+
+		if ($attemptsUsed->data >= 5) {
+		    _log()->log("Attempt to log in from banned IP address "
+		        . "[{$_SERVER['REMOTE_ADDR']}], username: "
+		        . "{$this->controller->form['username']}",FF_NOTICE);
+		}
+		return ($attemptsUsed->data >= 5);
     }
 
     private function processLogin() {
@@ -21,16 +46,34 @@ class LoginBox extends FPageFragment {
 		if ($data['username'] == '' || $data['password'] == '') {
 			$this->controller->set('lb_errorNoInfo',true);
 		}
+		
 		// Attempt to log in
 		if(FSessionManager::doLogin()) {
 			$this->controller->redirect($this->successURI);
 		} else {
+		    try {
+		        // Store the failed attempt in the logs
+    		    $q = "INSERT INTO `app_logs` (`created`,`ip`,`code`,`extra`) VALUES('".
+    		            date('Y-m-d g:i:s')."',".
+    		            "'{$_SERVER['REMOTE_ADDR']}',".
+    		            "101,".            // Furnace "Login Failed Event" code
+    		            "'{$data['username']}') ";
+    		    _db()->rawExec($q);
+		    } catch (FDatabaseException $e) {
+		        // Silently ignore if app_logs does not exist
+		    }
+		    
 			$this->controller->set('lb_loginError',true);
 		}
 	}
 
     public function render() {
-        return $this->getView('LoginBox');
+		if ($this->banned) {
+		    $this->controller->set('ip',$_SERVER['REMOTE_ADDR']);
+		    return $this->getView('Banned');
+		} else {
+            return $this->getView('LoginBox');
+		}
     }
 }
 ?>
