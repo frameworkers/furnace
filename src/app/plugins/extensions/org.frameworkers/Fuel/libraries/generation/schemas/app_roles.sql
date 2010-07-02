3c98ff70408e486445f16065792b8d5200d12d4c
Andrew Hart <andrew@frameworkers.org>
2010-07-02 14:03:31 +0000

    moved the FUEL extension out of the Furnace trunk and into its own project. It will still ship with each new Furnace download, but separating the two will allow development of each to proceed on different release cycles

--- src/app/plugins/extensions/org.frameworkers/Fuel/libraries/generation/schemas/app_roles.sql
+++
@@ -1,8 +0,0 @@
--- 
--- Table structure for table `app_roles`
--- 
-
-CREATE TABLE `app_roles` (
-  `faccount_id` int(10) unsigned NOT NULL,
-  PRIMARY KEY  (`faccount_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='permissions table for application accounts';
