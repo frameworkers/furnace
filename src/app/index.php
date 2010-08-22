aaa5748aa04ce7dc5df35b135f35a3380ea386fe
Andrew Hart <andrew@frameworkers.org>
2010-08-22 03:33:56 +0000

    support requests from the command line as well as from http

--- src/app/index.php
+++ src/app/index.php
@@ -1,11 +1,10 @@
 <?php
-
 /**
  * IF YOU HAVE MOVED YOUR FURNACE LIBRARY, OR WISH TO
  * POINT TO A DIFFERENT VERSION, MODIFY 'FURNACE_LIB_PATH'
  * TO POINT TO THE FULL PATH TO YOUR FURNACE INSTALLATION:
  */
-define("FURNACE_LIB_PATH", dirname(dirname(__FILE__)) . '/lib');
+define("FURNACE_LIB_PATH", '/home/andrew/Public/Frameworkers/frameworkers-furnace-trunk/src/lib');
 
 
 /*********************************************************
@@ -13,32 +12,43 @@ define("FURNACE_LIB_PATH", dirname(dirname(__FILE__)) . '/lib');
  *********************************************************/
 
 define("FURNACE_APP_PATH", dirname(__FILE__));
+define("CLI",              !isset($_SERVER['HTTP_USER_AGENT']));
 
 /* INCLUDE REQUIRED FILES */
- require('Log.php');
+require('Log.php');
 
- /* LOAD GLOBAL FUNCTIONS, STRUCTURES, AND DEFINITIONS */
- include(FURNACE_LIB_PATH . '/etc/globals.inc.php');
+/* LOAD GLOBAL FUNCTIONS, STRUCTURES, AND DEFINITIONS */
+include(FURNACE_LIB_PATH . '/etc/globals.inc.php');
  
- /* LOAD APPLICATION CONFIGURATION */
- $config  = new FApplicationConfig(Furnace::yaml(FF_CONFIG_FILE));
+/* LOAD APPLICATION CONFIGURATION */
+$config  = new FApplicationConfig(Furnace::yaml(FF_CONFIG_FILE));
  
- /* START UP THE APPLICATION LOG MANAGER */
- $_logmgr = new FApplicationLogManager($config);
- _log()->log("Processing request: {$_SERVER['REQUEST_URI']} from {$_SERVER['REMOTE_ADDR']}");
- 
- /* CREATE A REQUEST OBJECT */
- $request = new FApplicationRequest($_SERVER['REQUEST_URI']);
+/* START UP THE APPLICATION LOG MANAGER */
+$_logmgr = new FApplicationLogManager($config);
 
- /* INITIALIZE FURNACE */
- $furnace  = new Furnace($config);
+/* INITIALIZE FURNACE */
+$furnace  = new Furnace($config);
+$furnace->loadApplicationModel();
  
- /* PROCESS THE REQUEST OBJECT */
- $furnace->process($request);
+/* HANDLE A REQUEST THAT ORIGINATED FROM THE COMMAND LINE */
+if (CLI) {
+	_log()->log("Processing command line request: " . implode(' ',$argv) );
+}
  
- /* CLEAN UP */
- unset($furnace);
+/* HANDLE A REQUEST THAT ORIGINATED FROM */
+else {
+	_log()->log("Processing request: {$_SERVER['REQUEST_URI']} from {$_SERVER['REMOTE_ADDR']}");
+
+	/* CREATE A REQUEST OBJECT */
+	$request = new FApplicationRequest($_SERVER['REQUEST_URI']);
+	 
+	/* PROCESS THE REQUEST OBJECT */
+	$furnace->process($request);
  
- /* EXIT */
- exit();
+	/* CLEAN UP */
+	unset($furnace);
+	
+	/* EXIT */
+	exit();
+}
 ?>
