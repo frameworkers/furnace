1154f3f36ebbde5a75e9da21a8ab41a17c9ee1f3
Andrew Hart <andrew@frameworkers.org>
2011-02-10 06:28:18 +0000

    importing application skeleton [3/5]
    default controller and view, as well as 
    .htaccess and front-controller file

---
+++ app/index.php
@@ -0,0 +1,55 @@
+<?php
+
+define("FURNACE_VERSION","0.3.0");
+define("REQUEST_START",microtime(true));     // Request statistics
+
+/*
+ * The Front controller for this application
+ */
+// Furnace library location
+define("FURNACE_LIB_PATH",dirname(dirname(__FILE__))
+	."/lib/src");
+	
+// Furnace application location
+define("FURNACE_APP_PATH",dirname(__FILE__));
+
+// Initialize the Furnace Auto-loader
+require_once(FURNACE_LIB_PATH . 
+	'/org/frameworkers/furnace/utilities/AutoLoader.class.php');
+AutoLoader::init();
+
+// Furnace global application object
+function app() {
+	static $furnace;
+	
+	if (null == $furnace) {
+		$furnace = new \org\frameworkers\furnace\Furnace();
+	}
+	
+	return $furnace;
+}
+
+// Load the application configuration
+require(FURNACE_APP_PATH . "/config/application.config.php");
+
+// Load the application authentication configuration
+include(FURNACE_APP_PATH . "/config/authentication.config.php");
+
+// Load the application connection configuration
+include(FURNACE_APP_PATH . "/config/connections.config.php");
+
+// Load the application route definitions
+include(FURNACE_APP_PATH . "/config/routes.config.php");
+
+// Load the application content-type definitions
+include(FURNACE_APP_PATH . "/config/types.config.php");
+
+// Determine Application URL base
+$urlbase = str_replace($_SERVER['DOCUMENT_ROOT'],'',$_SERVER['SCRIPT_FILENAME']);
+$urlbase = str_replace('/index.php','',$urlbase);
+
+org\frameworkers\furnace\config\Config::Set('applicationUrlBase',$urlbase);
+
+// Handle the request
+app()->processRequest($_SERVER['REQUEST_URI']);
+
