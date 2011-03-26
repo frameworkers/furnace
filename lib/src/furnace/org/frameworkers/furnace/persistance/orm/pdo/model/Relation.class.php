86dc23c41507ee76c1e0f533cb52b3933aa7244e
Andrew Hart <andrew@frameworkers.org>
2011-03-26 16:40:34 +0000

    updates to the Relation class to parse new model format

--- lib/src/furnace/org/frameworkers/furnace/persistance/orm/pdo/model/Relation.class.php
+++ lib/src/furnace/org/frameworkers/furnace/persistance/orm/pdo/model/Relation.class.php
@@ -19,109 +19,39 @@ namespace org\frameworkers\furnace\persistance\orm\pdo\model;
  */
 class Relation {
 	
-	const ORM_RELATION_HASMANY=   'Has Many';
-	const ORM_RELATION_BELONGSTO= 'Belongs To';
+	const ORM_RELATION_ONE  = '1';
+	const ORM_RELATION_MANY = 'M';
 	
+	// Relationship metadata
 	public $localObjectClassName;
 	public $remoteObjectClassName;
 	
-	public $name;
-	public $metadata;
-	
-	public $type;
-	public $isRequired = false;
-	public $isPrimary  = false;
-	
 	public $localObjectTable;
+	public $localObjectColumn;
+	
+	public $remoteObjectLabel;
 	public $remoteObjectTable;
+	public $remoteObjectColumn;
 	
-	public $lookupObject;
-	public $reciprocalRelation;
+	public $lookupObjectClassName;
+	public $lookupObjectTable;
+	public $lookupObjectLocalRel;
+	public $lookupObjectRemoteRel;
 	
-	public $localKeyColumn;
-	
-	public $remoteKeyAttr;
-	public $remoteKeyColumn;
-	
-	public function __construct($name,$objOwner,$data = array()) {
-		// Store the name for this relation
-		$this->name     = Lang::ToAttributeName($name);
-		$this->metadata = $data;
-		
-		// Store type information
-		$this->type = $data['type'];
-		
-		// Store whether this relation is required or not
-		$this->isRequired = (isset($data['required']) || isset($data['primary']));
-		
-		// Store whether this relation is a primary key for the local object
-		$this->isPrimary  = (isset($data['primary']));
-		
-		// Store the owner object information
-		$this->localObjectClassName  = $objOwner->className;
-		$this->remoteObjectClassName = $data['remoteObjectClassName'];		
-		
-		// ORM_RELATION_HASMANY
-		if ($this->type == Relation::ORM_RELATION_HASMANY) {
-			// This is a M-M relation with an intermediate lookup object
-			if (isset($data['via'])) {
-				// Determine lookup information
-				$this->lookupObject = Lang::ToClassName($data['via'],true);
-				// Determine local key column    -- there is none, maybe local  lookup key column
-				// Determine remote key column   -- there is none, maybe remote lookup key column
-				
-				
-			}
-			// This is a 1-M relation
-			// These can ONLY be generated *internally* in response to the
-			// modeler creating a "Belongs To" relation and specifying a 
-			// reciprocal name.
-			else {
-				// Determine lookup information  -- there is none
-				// Determine local key column    -- there is none
-				// Determine remote key column
-				$this->remoteKeyColumn =  $data['remoteKeyColumn'];
-			}
-			
-		// ORM_RELATION_BELONGSTO	
-		} else {
+	public $localName;
+	public $localType;
+	public $locallyRequired  = true;
+	public $locallyPrimary   = false;
+	public $remoteName;
+	public $remoteType;
+	public $remotelyRequired = false;
+	public $remotelyPrimary  = false;
 
-			if (isset($data['key'])) {
-				// Explicitly overriding the foreign key. Also check if there are
-				// commas in the provided value, meaning we have to generate more
-				// than one column on the local object to fully map to all remote keys!
-			} else {
-				// Primary key detection on the remote object to determine the 
-				// remote key.
-				$remotePrimaryKeys = Model::Get()->PrimaryKeysFor($this->remoteObjectClassName,$this->localObjectClassName);
-
-				// The remote object has multiple primary keys defined
-				if (count($remotePrimaryKeys['attrKeys']) + count($remotePrimaryKeys['relKeys']) > 1) {
-					die("not implemented yet Relation.class.php:87");
-				}
-				
-				// The remote object has EXACTLY 1 primary key defined, then
-				else {
-					// If the key corresponds to an attribute
-					if (count($remotePrimaryKeys['attrKeys']) == 1) {
-						$this->remoteKeyColumn = $remotePrimaryKeys['attrKeys'][0]->column;
-						$this->remoteKeyAttr   = $remotePrimaryKeys['attrKeys'][0];
-					}
-					//   Else the key corresponds to a relation
-					else {
-						$this->remoteKeyColumn = $remotePrimaryKeys['relKeys'][0]->localKeyColumn;
-					}
-					// In either case, a local key column needs to be generated:
-					$this->localKeyColumn  = new Column(
-						$this->remoteKeyColumn->name,
-						$this->remoteKeyColumn->type,
-						false,
-						true,
-						$this->remoteKeyColumn->extra);
-				}
-			}
-		}
+	
+	public function __construct($name) {
+		$this->localName = Lang::ToAttributeName($name);
 	}
+	
 	
 	public function getReciprocalRelation() {
 		if ($this->reciprocalRelation) {
