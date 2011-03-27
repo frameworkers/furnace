fe973c393d77ab0eb3334a8a485614418fa9e4b6
Andrew Hart <andrew@frameworkers.org>
2011-03-27 16:40:02 +0000

    improvements to the inflector

--- lib/src/furnace/org/frameworkers/furnace/persistance/orm/pdo/model/Inflection.class.php
+++ lib/src/furnace/org/frameworkers/furnace/persistance/orm/pdo/model/Inflection.class.php
@@ -9,6 +9,11 @@ class Inflection extends StaticObject {
 	
 	public static function ToSingular($string) {
 		
+		// Make singular by replacing 'ies' -> 'y'
+		if ('ies' == substr($string,-3,3)) {
+			return substr($string,0,-3) . 'y';
+		}
+		
 		// Make singular by dropping the terminating 's'
 		if ('s' == substr($string,-1,1)) {
 			return substr($string,0,-1);
