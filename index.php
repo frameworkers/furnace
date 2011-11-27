ea0cae73f0633090cff4bcc55660c3c109408a33
Andrew Hart <andrew@frameworkers.org>
2011-11-27 21:32:47 +0000

    bump version for release

--- index.php
+++ index.php
@@ -31,7 +31,7 @@
  ***/
 
 // 0.1 Define Constants --------------------------------------------------------------------
-define('FURNACE_VERSION'      , '0.4.0');
+define('FURNACE_VERSION'      , '0.4.1');
 define('F_REQUEST_START'      , microtime(true));
 define('F_START_MEMORY'       , memory_get_usage());
 define('F_ENV_PRODUCTION'     , 1000);
