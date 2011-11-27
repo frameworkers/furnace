d8a7c4febf7db447fb97dddba361bfd54358f8c2
Andrew Hart <andrew@frameworkers.org>
2011-11-27 20:31:42 +0000

    add a simple index view with a link to the 'first steps' tutorial

---
+++ modules/app/views/index.php
@@ -0,0 +1,16 @@
+<?php 
+?>
+<div style="width:700px;margin:20px auto;">
+    <center>
+    <h1>Welcome To Furnace</h1>
+    <h2>You are running version <?php echo FURNACE_VERSION?></h2>
+    <br/>
+    <p>
+    If this is your first Furnace application,
+    take a look at the "First Steps with Furnace" tutorial for this version, available online at: <br/><br/>
+    <a href="http://furnace.frameworkers.org/docs/<?php echo FURNACE_VERSION?>/tutorial-01-first-steps-with-furnace">
+        http://furnace.frameworkers.org/docs/<?php echo FURNACE_VERSION?>/tutorial-01-first-steps-with-furnace
+    </a>
+    </p>
+    </center>
+</div>
