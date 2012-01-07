7ff8bfc66609ddfbed392edc1f54de863336c3b4
Andrew Hart <andrew@frameworkers.org>
2012-01-07 03:03:39 +0000

    cleanup (formatting)

--- lib/furnace/routing/Router.class.php
+++ lib/furnace/routing/Router.class.php
@@ -152,119 +152,119 @@ class Router {
 			$url = substr($url,0,$qmark);
 		}
 
-        // Strip the prefix (url_base) from the incoming url
-        $prefix = Config::Get('applicationUrlBase');
-        if ($prefix != '/') {
-        	$url    = substr($url,strlen($prefix) + 1);
-        }
-        
-        // Break the incoming url into its segmented parts
-        $parts  = explode('/',ltrim($url,'/'));
+    // Strip the prefix (url_base) from the incoming url
+    $prefix = Config::Get('applicationUrlBase');
+    if ($prefix != '/') {
+    	$url    = substr($url,strlen($prefix) + 1);
+    }
+    
+    // Break the incoming url into its segmented parts
+    $parts  = explode('/',ltrim($url,'/'));
 
-        // Look for a response type which can be specified by the presence of
-        // an extension on the request (e.g.: /blog/post/34.json => 'json').
-        // Any usable type must have a corresponding entry in types.config.php
-        // or it will be ignored.
-        $type     = null;
-        $lastPart =& $parts[count($parts)-1];
-        $foundDot = strrpos($lastPart,'>') !== false;
-        $ext      = ($foundDot) 
-        	? substr($lastPart,strrpos($lastPart,'>') + 1) 
-        	: false;
-        if ( $ext && ResponseTypes::TypeExists( $ext ) ) {
-        	$type = $ext;
-        }
-        if ( $ext ) { // Strip the extension from the route
-        	$lastPart = str_replace(">{$ext}",'',$lastPart);
-        }
+    // Look for a response type which can be specified by the presence of
+    // an extension on the request (e.g.: /blog/post/34.json => 'json').
+    // Any usable type must have a corresponding entry in types.config.php
+    // or it will be ignored.
+    $type     = null;
+    $lastPart =& $parts[count($parts)-1];
+    $foundDot = strrpos($lastPart,'>') !== false;
+    $ext      = ($foundDot) 
+    	? substr($lastPart,strrpos($lastPart,'>') + 1) 
+    	: false;
+    if ( $ext && ResponseTypes::TypeExists( $ext ) ) {
+    	$type = $ext;
+    }
+    if ( $ext ) { // Strip the extension from the route
+    	$lastPart = str_replace(">{$ext}",'',$lastPart);
+    }
 
-        $the_route = array();
-        
-        foreach ( self::$routes as $route ) {
-            Logger::Log(LogLevel::DEBUG,"Trying route: {$route['url']}");
-        	            
-            $rp = explode('/',ltrim($route['url'],'/'));
-            $wildcards  = array();
-            $parameters = array();
-            
-            // If the number of defined segments is greater than
-            // the provided segments, ignore this route
-            if ( count($rp) > count($parts) ) {
-              continue;
-            }
-            
-            // If route is '/', both will contain empty strings
-            // Clear it out to bypass following wildcard test
-            if (empty($rp[0]) && empty($parts[0])) {
-            	$rp = $parts = array();
-            }
-            
-            // Test each non-wildcard part for a match
-            // Wildcards are * and :text
-            $matched = true;
-            for ($j = 0, $c = count($rp); $j < $c; $j++ ) {
-            	
-              // Just ignore asterisk wildcards (don't care)
-              if ($rp[$j] == '*') {
-                continue;
-              }
-              // Capture named wildcards in the 'wildcards' array
-              if ($rp[$j] && $rp[$j][0] == ':') {
-                $wildcards[substr($rp[$j],1)] = $parts[$j];
-                if ( ':handler'    != $rp[$j] && 
-            	     ':controller' != $rp[$j] ) {
-                  $parameters[substr($rp[$j],1)] = $parts[$j];
-                }
-                continue;
-              }
-              // Test for equality between non-wildcard parts
-              if ($rp[$j] != $parts[$j]) {
-                $matched = false;
-                break;
-              }
-            }
-            
-            if ( $matched ) {
-              // Capture additional view arguments (in the case that the 
-              // url contained more parts than the matching route rule
-              for ( $k = count($rp), $l = count($parts); $k < $l; $k++ ) {
-                if ('' != $parts[$k] ) { $parameters[] = $parts[$k]; }
-              }
-                          
-              // Build the resulting route data array
-              $the_route = array(
-                'url'        => $url,
-                'route'      => $route['url'],
-                'prefix'     => $prefix,
-                'extension'  => isset($route['extension']) ? $route['extension'] : false,
-                'module'     => isset($route['module']) ? $route['module'] : 'default',
-                'layout'     => isset($route['layout']) ? $route['layout'] : Config::Get('default.layout'),
-                'controller' => ((isset($wildcards['controller']) && 
-            		             !empty($wildcards['controller']))
-                   ? $wildcards['controller']
-                   : (isset($route['controller'])
-                      ? $route['controller']
-                      : 'default')),
-                'handler' => ((isset($wildcards['handler']) &&
-                              !empty($wildcards['handler']))
-                   ? $wildcards['handler']
-                   : (isset($route['handler'])
-                     ? $route['handler']
-                     : 'index')),
-                'parameters' => $parameters,
-                'type'       => ((isset($type)    // has an extension been detected?
-                     ? $type                      // use it, otherwise:
-                     : (isset($route['type'])     // is there type information in the route?
-                     	? $route['type']          // use it, otherwise:
-                     	: 'text/html'))), // use the default configured type
-              );
-              
-              Logger::Log(LogLevel::INFO,"Matched route: {$the_route['route']}");
-              return new Route($the_route);
-            }
+    $the_route = array();
+    
+    foreach ( self::$routes as $route ) {
+      Logger::Log(LogLevel::DEBUG,"Trying route: {$route['url']}");
+  	            
+      $rp = explode('/',ltrim($route['url'],'/'));
+      $wildcards  = array();
+      $parameters = array();
+      
+      // If the number of defined segments is greater than
+      // the provided segments, ignore this route
+      if ( count($rp) > count($parts) ) {
+        continue;
+      }
+      
+      // If route is '/', both will contain empty strings
+      // Clear it out to bypass following wildcard test
+      if (empty($rp[0]) && empty($parts[0])) {
+      	$rp = $parts = array();
+      }
+      
+      // Test each non-wildcard part for a match
+      // Wildcards are * and :text
+      $matched = true;
+      for ($j = 0, $c = count($rp); $j < $c; $j++ ) {
+      	
+        // Just ignore asterisk wildcards (don't care)
+        if ($rp[$j] == '*') {
+          continue;
         }
+        // Capture named wildcards in the 'wildcards' array
+        if ($rp[$j] && $rp[$j][0] == ':') {
+          $wildcards[substr($rp[$j],1)] = $parts[$j];
+          if ( ':handler'    != $rp[$j] && 
+      	     ':controller' != $rp[$j] ) {
+            $parameters[substr($rp[$j],1)] = $parts[$j];
+          }
+          continue;
+        }
+        // Test for equality between non-wildcard parts
+        if ($rp[$j] != $parts[$j]) {
+          $matched = false;
+          break;
+        }
+      }
+      
+      if ( $matched ) {
+        // Capture additional view arguments (in the case that the 
+        // url contained more parts than the matching route rule
+        for ( $k = count($rp), $l = count($parts); $k < $l; $k++ ) {
+          if ('' != $parts[$k] ) { $parameters[] = $parts[$k]; }
+        }
+                    
+        // Build the resulting route data array
+        $the_route = array(
+          'url'        => $url,
+          'route'      => $route['url'],
+          'prefix'     => $prefix,
+          'extension'  => isset($route['extension']) ? $route['extension'] : false,
+          'module'     => isset($route['module']) ? $route['module'] : 'default',
+          'layout'     => isset($route['layout']) ? $route['layout'] : Config::Get('default.layout'),
+          'controller' => ((isset($wildcards['controller']) && 
+      		             !empty($wildcards['controller']))
+             ? $wildcards['controller']
+             : (isset($route['controller'])
+                ? $route['controller']
+                : 'default')),
+          'handler' => ((isset($wildcards['handler']) &&
+                        !empty($wildcards['handler']))
+             ? $wildcards['handler']
+             : (isset($route['handler'])
+               ? $route['handler']
+               : 'index')),
+          'parameters' => $parameters,
+          'type'       => ((isset($type)    // has an extension been detected?
+               ? $type                      // use it, otherwise:
+               : (isset($route['type'])     // is there type information in the route?
+               	? $route['type']          // use it, otherwise:
+               	: 'text/html'))), // use the default configured type
+        );
         
-        return false;
+        Logger::Log(LogLevel::INFO,"Matched route: {$the_route['route']}");
+        return new Route($the_route);
+      }
+    }
+    
+    return false;
 	}
 	
 	/**
