3c1f94bfe92b244c6c306f1ec68dbb87e5e6fd9e
Andrew Hart <andrew@datafluency.com>
2012-04-23 08:00:38 -0700

    bump version for release

--- index.php
+++ index.php
@@ -31,7 +31,7 @@
  ***/
 
 // 0.1 Define Constants --------------------------------------------------------------------
-define('FURNACE_VERSION'      , '0.4.3');
+define('FURNACE_VERSION'      , '0.4.4');
 define('F_REQUEST_START'      , microtime(true));
 define('F_START_MEMORY'       , memory_get_usage());
 define('F_ENV_PRODUCTION'     , 1000);
